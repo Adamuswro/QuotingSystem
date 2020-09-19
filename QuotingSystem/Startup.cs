6b286967ddfa58e0dc0ccbddf5a3d166e6e98549
Adam <adamszulc1991@gmail.com>
2020-09-19 23:53:17 +0200

    Init commit

---
+++ QuotingSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuotingSystem.Startup))]
+namespace QuotingSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
